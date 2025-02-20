b76b3118040dc52a5b19e3dc0d48115b25e82a94
aatifali10 <aatifwarraich10@gmail.com>
2025-02-20 22:50:51 +0500

    first server created successfully and 5 jokes

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require("express");
-require("dotenv").config();
+import express from "express";
 
 const app = express();
+const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Today is my first server build. I'm so exicted");
+app.get("/jokes", (req, res) => {
+  const jokes = [
+    { id: 1, name: "Joke No 1", description: "Joke is nothing" },
+    { id: 2, name: "Joke No 2", description: "Second Joke" },
+    { id: 3, name: "Joke No 3", description: "Third Joke" },
+    { id: 4, name: "Joke No 4", description: "Fourth Joke" },
+    { id: 5, name: "Joke No 5", description: "Fifth Joke" },
+  ];
+  res.json(jokes);
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`server listen http://localhost:${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`server listen on http://localhost:${port}`);
 });
