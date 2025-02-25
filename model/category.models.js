8267162f7364d8abc877bf8dae9a8622bd41cc5b
aatifali10 <aatifwarraich10@gmail.com>
2025-02-25 23:20:34 +0500

    folder strcuture

--- model/category.models.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const categorySchema= new mongoose.Schema({
-
-},{timestamps:true})
-
-export const Category= mongoose.model("Category",categorySchema);
--- model/products.models.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-const productSchema = new mongoose.Schema({}, { timestamps: true });
-
-export const Product = mongoose.model("Product", productSchema);
